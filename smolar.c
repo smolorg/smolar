2be3bceb9053d9c3596a4d082cb0aeea95cb2da5
Maharshi Pandya <maharshipandya@192.168.1.5>
2024-07-04 11:45:52 +0530

    matmul test

--- smolar.c
+++ smolar.c
@@ -926,23 +926,37 @@ int main()
     // new random values
     srand(time(NULL));
 
-    int shape_a[] = {2, 3, 4};
-    int shape_b[] = {2, 4, 2};
+    int shape_a[] = {100, 100};
+    int shape_b[] = {100, 100};
 
-    Array *a = smReshapeNew(smArange(1, 25, 1), shape_a, 3);
-    Array *b = smReshapeNew(smArange(1, 17, 1), shape_b, 3);
+    int num_runs = 5;
+    clock_t start, end;
+    float cpu_time_used, total_time = 0.0f;
 
-    Array *res = smMatMul(a, b);
+    Array *a = smReshapeNew(smArange(1, 10001, 1), shape_a, 2);
+    Array *b = smReshapeNew(smArange(1, 10001, 1), shape_b, 2);
 
-    printf("Array a:\n");
-    smShow(a);
-    printf("Array b:\n");
-    smShow(b);
-    printf("\nMatmul result:\n");
-    smShow(res);
+    printf("\nA shape:\n");
+    __printArrayInternals__(a, a->shape);
+    printf("B shape:\n");
+    __printArrayInternals__(b, b->shape);
+
+    for(int i=0; i<num_runs; i++)
+    {
+        start = clock();
+        Array *res = smMatMul(a, b);
+        end = clock();
+
+        cpu_time_used = ((float) (end - start)) / CLOCKS_PER_SEC;
+        total_time += cpu_time_used;
+
+        smCleanup(res);
+    }
+    
+    float avg_time = total_time / num_runs;
+    printf("\naverage execution time for matmul over %d runs: %f seconds\n\n", num_runs, avg_time);
 
     smCleanup(a);
     smCleanup(b);
-    smCleanup(res);
     return 0;
 }
